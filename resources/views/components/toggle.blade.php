0c8421ef3b2f10c041bb891994e12ce2b8f535c0
thinh1995 <cuongthinhtuan2006@gmail.com>
2025-03-13 12:09:02 +0700

    master: update logic toggle

--- resources/views/components/toggle.blade.php
+++ resources/views/components/toggle.blade.php
@@ -3,13 +3,14 @@
 	'value' => null,
 	'label' => null,
 	'description' => null,
-	'inline' => false
+	'inline' => false,
+  'checked' => false,
 ])
 
 <label class="form-check form-switch @if($inline) form-check-inline @endif">
   <input name="{{ $name }}" type="hidden" value="0">
   <input {{ $attributes->merge(['class' => 'form-check-input']) }} type="checkbox" id="{{ $id }}" name="{{ $name }}"
-         value="{{ $value }}">
+         value="{{ $value }}" @checked($checked)>
   @if(!is_null($label))
     <span class="form-check-label">{{ $label }}</span>
   @endif
