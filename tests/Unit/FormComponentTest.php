0433c0a4b8a3c022657b26d8f604d79cff91f4c1
thinh1995 <cuongthinhtuan2006@gmail.com>
2025-03-18 16:22:48 +0700

    master: add test Radio, Form component

---
+++ tests/Unit/FormComponentTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lucifer\LaravelTablerUi\Tests\Unit;
+
+use Lucifer\LaravelTablerUi\Tests\TestCase;
+
+class FormComponentTest extends TestCase
+{
+    public function test_get_form_can_be_rendered()
+    {
+        $view = $this->blade('<x-tabler-form class="form-check" action="/" method="get"></x-tabler-form>');
+
+        $view->assertSee('action="/"', false);
+        $view->assertSee('method="get"', false);
+        $view->assertSee('class="form-check"', false);
+    }
+
+    public function test_post_form_can_be_rendered()
+    {
+        $view = $this->blade('<x-tabler-form action="/" method="post" enctype="multipart/form-data"></x-tabler-form>');
+
+        $view->assertSee('action="/"', false);
+        $view->assertSee('method="post"', false);
+        $view->assertSee('enctype="multipart/form-data"', false);
+    }
+}
---
+++ tests/Unit/RadioComponentTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lucifer\LaravelTablerUi\Tests\Unit;
+
+use Lucifer\LaravelTablerUi\Tests\TestCase;
+
+class RadioComponentTest extends TestCase
+{
+    public function test_basic_radio_can_be_rendered()
+    {
+        $view = $this->blade('<x-tabler-radio label="Status" name="status" value="1" description="Status of model"/>');
+
+        $view->assertSee('name="status"', false);
+        $view->assertSee('value="1"', false);
+        $view->assertSee('Status of model');
+    }
+
+    public function test_inline_radio_can_be_rendered()
+    {
+        $view = $this->blade('<x-tabler-radio label="Status" name="status" value="1" :inline="true"/>');
+
+        $view->assertSee('form-check-inline');
+    }
+}
